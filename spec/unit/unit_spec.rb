c0ec5eda437022c959a26c32c54effca0bb9410e
Mario Morelos <mmorelos2@tamu.edu>
2023-09-05 23:06:33 -0500

    Book Collection 2 - Added tests for new book attributes

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(
+        title: 'harry potter',
+        author: 'Some guy',
+        price: 29.99,
+        published_date: Date.new(2023, 9, 4)
+      )
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +17,21 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without published_date' do
+    subject.published_date = nil
     expect(subject).not_to be_valid
   end
 end
